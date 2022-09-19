7452ee1d78f528d6765cd4ac2343414b24441bca
ViniSSantos <cepef.rh@gmail.com>
2022-09-19 09:12:13 -0300

    frist commit

---
+++ scr/index.js
@@ -0,0 +1,13 @@
+scrolled = (nav) => {
+    nav.classList.toggle('scrolled', window.scrollY > 50);
+  };
+  
+  window.onload = () => {
+    const nav = document.getElementById('navbar-main');
+  
+    scrolled(nav);
+  
+    window.onscroll = () => {
+      scrolled(nav);
+    };
+};
